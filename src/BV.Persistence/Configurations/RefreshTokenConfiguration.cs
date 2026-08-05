755f6b5d84b2c68ac2c381a686602640f704324f
veisozgur <veisozgur@bvteknoloji.com.tr>
2026-08-05 23:18:43 +0300

    Map refresh tokens

---
+++ src/BV.Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,17 @@
+using BV.Domain.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BV.Persistence.Configurations;
+
+public sealed class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.ToTable("RefreshTokens");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.TokenHash).HasMaxLength(128).IsRequired();
+        builder.HasIndex(x => x.TokenHash).IsUnique();
+        builder.HasIndex(x => x.UserId);
+    }
+}
